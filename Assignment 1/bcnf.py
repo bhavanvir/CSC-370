6f797408c04434fd23283894c34f8a739a85f65f
Bhavanvir Rai <bhavanvir.r@gmail.com>
2022-05-20 12:06:40 -0700

    Update bcnf.py

--- Assignment 1/bcnf.py
+++ Assignment 1/bcnf.py
@@ -7,7 +7,6 @@ from functional_dependency import *
 # in the ImplementMe class and submit this (and only this!) file.
 # You are welcome to add supporting classes and methods in this file.
 class ImplementMe:
-
     # Returns the number of recursive steps required for BCNF decomposition
     #
     # The input is a set of relations and a set of functional dependencies.
@@ -16,35 +15,32 @@ class ImplementMe:
     # decomposition or -1 if the relations are not a correct decomposition.
     @staticmethod
     def DecompositionSteps( relations, fds ):
-        dependency_set = fds.functional_dependencies
-        lhs, rhs = [], []
-        for functional_dependency in dependency_set:
-            lhs.append(functional_dependency.left_hand_side)
-            rhs.append(functional_dependency.right_hand_side)
-
-        relation_set = relations.relations
+        fdep = fds.functional_dependencies
+        fset = [[] for i in range(len(fdep))]
+        print(fset[0])
+        i = 0
+        for fd in fdep:
+            to_close = fd.left_hand_side.union(fd.right_hand_side)
+            if i < len(fdep):
+                fset[i] = (closure(fds, to_close))
+                i += 1
+        print(fset)
+        
+        """ relation_set = relations.relations
         relations = []
         for relation in relation_set:
-            relations.extend(relation.attributes)
-        relations = set(relations)
-
-        combined = [[] for i in range(len(lhs))]
-        for i in range(len(lhs)):
-            combined[i].extend(lhs[i])
-            combined[i].extend(rhs[i])
-
-        """
-        Compute the intersection between all pairs of items in the combined list,
-        then union the intersection set with the appropriate FD
-        """
-        difference = []
-        for i in range(len(combined)):
-            if i + 1 < len(combined):
-                difference.append(set(combined[i]).intersection(combined[i + 1]))
-        print(combined, "AND", difference)
-                
-        closure_diff = [[] for i in range(len(lhs))]
-        for i in range(len(lhs)):
-            closure_diff[i] = (relations.difference(combined[i])).union(lhs[i])
-
+            relations.extend(relation.attributes) """
+        
         return 500
+
+def closure(fds, dependency):
+    fdep = fds.functional_dependencies
+    result = set(dependency)
+    more = True
+    while more:
+        more = False
+        for fd in fdep:
+            if fd.left_hand_side.issubset(result) and not fd.right_hand_side.issubset(result):
+                result.update(fd.right_hand_side)
+                more = True
+    return result
