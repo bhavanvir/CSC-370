1e85b7344c0bdd5a9e80126fa94f59ce4e7d0457
Bhavanvir Rai <bhavanvir.r@gmail.com>
2022-07-12 18:46:58 -0700

    Add files via upload

--- Assignment 3/query06.sql
+++ Assignment 3/query06.sql
@@ -7,4 +7,27 @@
 -- 0.9 marks: <20 operators
 -- 0.8 marks: correct answer
 
--- Replace this comment line with the actual query
+WITH T1 AS(
+    SELECT fips, cname AS name, abbr, snow, employees
+    FROM(
+        SELECT fips, name AS cname, abbr, snow FROM county
+        JOIN state ON state.id = state
+        WHERE county.name LIKE 'iron%' OR county.name LIKE 'mineral%' OR county.name LIKE 'coal%'
+    ) AS T
+    JOIN countyindustries ON county = fips
+    JOIN industry ON industry = id
+    WHERE name = 'Mining, quarrying, and oil and gas extraction'
+), T2 AS(
+    SELECT fips, name, abbr, snow, NULL AS employees FROM county
+    JOIN state ON state.id = state
+    WHERE county.name LIKE 'iron%' OR county.name LIKE 'mineral%' OR county.name LIKE 'coal%'
+)
+SELECT name, abbr, employees
+FROM(
+    SELECT name, abbr,snow, employees FROM T2
+    WHERE T2.fips NOT IN (SELECT fips FROM T1)
+    UNION
+
+    SELECT name, abbr, snow, employees FROM T1
+) AS T
+ORDER BY snow 
