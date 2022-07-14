6de87a0684d663274ce788a373ff80f343ad5b5c
Bhavanvir Rai <bhavanvir.r@gmail.com>
2022-07-13 19:10:29 -0700

    Slight issue with certain values in "% of Population" column.

--- Assignment 3/query09.sql
+++ Assignment 3/query09.sql
@@ -7,7 +7,12 @@
 -- 0.9 marks: <35 operators
 -- 0.8 marks: correct answer
 
-SELECT s.abbr, i.name FROM county c 
-JOIN countyindustries r ON r.county = c.fips
-JOIN state s ON s.id = c.state
+SELECT s.abbr, i.name, SUM(r.payroll) AS 'Total Payroll', (SUM(r.employees) / SUM(p.population) * 100) AS '% of Population' FROM countypopulation p
+JOIN county c ON p.county = c.fips
+JOIN state s ON c.state = s.id
+JOIN countyindustries r ON p.county = r.county
 JOIN industry i ON r.industry = i.id
+WHERE year = 2019 AND s.id != 49
+GROUP BY c.state, r.industry
+HAVING SUM(r.employees) / SUM(p.population) * 100 >= 7.5
+ORDER BY SUM(r.payroll) DESC;
